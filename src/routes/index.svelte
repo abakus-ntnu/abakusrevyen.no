5cad6dd16f99787598036ba1c66d960da0429b3f
Kristian <kristianint@outlook.com>
2022-01-30 17:49:39 +0100

    namedrop & promo vid 2022

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,10 @@
 <script>
-	import Banner from '../components/banner.svelte';
+	import Revy from '../components/revy.svelte';
+	import Background from '../components/background.svelte';
+
+	let videos = [
+		{ title: 'Promo Video', title_long: '2022 Promo Video', id: 'L9dHCyfT5OA' },
+	];
 </script>
 
 <style>
@@ -9,42 +14,14 @@
 </style>
 
 <svelte:head>
-	<title>Opptak til Abakusrevyen 2022!</title>
+	<title>Abakusrevyen 2022: Svin På Skogen!</title>
 </svelte:head>
 
-<div class="content">
-	<Banner boxed banner_link="https://tinyurl.com/abakusrevyen2022" />
-
-	<!-- Description -->
-	<h1>Abakusrevyen 2022 har opptak!</h1>
-	<p>
-		Har du lyst til å lage tidenes show på Byscenen den 3 og 4 Mars? <br />
-		Har du lyst til å skape minner for livet sammen andre flinke, <br />
-		morsomme og hyggelige abakuler? Da må du søke Abakusrevyen! <br />
-		Det er mulig å søke på så mange grupper som du vil, så ikke vær beskjeden! ;)
-	</p>
-	<br />
-	<div class="block">
-		<a class="button blue big wide" href="https://tinyurl.com/abakusrevyen2022">Søk Her!</a>
-	</div>
-	<br />
-	<p>
-		For informasjon om grupper og stillinger, se
-		<a href="https://abakus.no/articles/362/">denne</a>
-		artikkelen på abakus.no.
-	</p>
-	<p>
-		Spørsmål om alt og ingenting kan rettes til
-		<a href="mailto:revy@abakus.no">revy@abakus.no</a>
-		<br />
-		<b>
-			<u>Søknadsfrist 17. september!</u>
-		</b>
-	</p>
-	<p>
-		For mer info om fjorårets revy, se
-		<a href="/2021">Abakusrevyen 2021</a>.
-	</p>
-	<br />
-	<p>Sjekk også ut våre tideligere revyer i arkivet vårt <a href="/arkiv">her</a>.</p>
+<div>
+	<Revy image_format="images/2022/logo.png" {videos}>
+		<h2><i>3. og 4. mars braket det løs live fra Byscenen!</i></h2>
+		<h3>Nytt av i år blir det både fysisk publikum og digital stream slik at alle kan få med seg sesongens beste revy!</h3>
+	</Revy>
+	<p>Sjekk også ut våre andre revyer i arkivet vårt <a href="/arkiv">her</a>.</p>
 </div>
+<Background color="#78031e" />
