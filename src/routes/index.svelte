25b9901de639569dd0c62ce7c7f35e642b78f04b
Marcus Frenje <marcusfrenje25@gmail.com>
2022-02-02 08:54:02 +0100

    Add transparent logo, repeating background and new font

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,13 +3,16 @@
 	import Background from '../components/background.svelte';
 
 	let videos = [
-		{ title: 'Promo Video', title_long: '2022 Promo Video', id: 'L9dHCyfT5OA' },
+		{ title: 'Promovideo', title_long: '2022 Promovideo', id: 'L9dHCyfT5OA' },
 	];
 </script>
 
 <style>
 	div {
 		text-align: center;
+	}
+	h1, h2, h3{
+		font-family: "DBB";
 	}
 </style>
 
@@ -18,10 +21,16 @@
 </svelte:head>
 
 <div>
-	<Revy image_format="images/2022/logo.png" {videos}>
+	<h1>Abakusrevyen 2022 presenterer...</h1>
+	<Revy image_format="images/2022/logo.png" banner_link="https://youtu.be/L9dHCyfT5OA" {videos}>
 		<h2><i>3. og 4. mars braker det løs live fra Byscenen!</i></h2>
 		<h3>Nytt av i år blir det både fysisk publikum og digital stream slik at alle kan få med seg sesongens beste revy!</h3>
 		<h2><i>Billetter slippes snart så følg med!</i></h2>
 	</Revy>
+	<p>
+		Spørsmål om alt og ingenting kan rettes til
+		<a href="mailto:revy@abakus.no">revy@abakus.no</a>
+		<br />
+	</p>
 </div>
-<Background color="#78031e" />
+<Background color="#78031e" background_image="/images/2022/background-color.png"/>
